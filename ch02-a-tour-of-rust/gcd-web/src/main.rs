3d33bfdf69e1a35ab6adf385e32f9932fd605dd5
francoposa <franco@francoposa.io>
2021-07-24 09:02:52 -0700

    ch02 serving pages to the web - serving form and handling post

--- ch02-a-tour-of-rust/gcd-web/src/main.rs
+++ ch02-a-tour-of-rust/gcd-web/src/main.rs
@@ -1,8 +1,15 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+mod gcd;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let app = || App::new().route("/", web::get().to(get_index));
+    let app = || {
+        App::new()
+            .route("/", web::get().to(get_calc))
+            .route("/gcd", web::post().to(post_gcd))
+    };
     let server = HttpServer::new(app)
         .bind("127.0.0.1:8080")
         .expect("failed to bind on 127.0.0.1:8080");
@@ -15,4 +22,39 @@ fn get_index() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html")
         .body("Hello World")
+}
+
+fn get_calc() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+            <title>GCD Calculator</title>
+            <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+            </form>
+            "#,
+    )
+}
+
+#[derive(Deserialize)]
+struct GCDParams {
+    n: u64,
+    m: u64,
+}
+
+fn post_gcd(form: web::Form<GCDParams>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Come on now... you know you can't use zero");
+    }
+    let body = format!(
+        "The Greatest Common Divisor of {} and {} \
+        is <b>{}</b>\n",
+        form.n,
+        form.m,
+        gcd::gcd(form.n, form.m)
+    );
+    HttpResponse::Ok().content_type("text/html").body(body)
 }
