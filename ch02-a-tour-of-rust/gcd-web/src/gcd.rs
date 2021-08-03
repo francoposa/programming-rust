d67333c31cb5d3bc4ee15b236486b0c824342353
francoposa <franco@francoposa.io>
2021-08-02 20:04:42 -0700

    ch02 serving pages to the web - serving form and handling post

---
+++ ch02-a-tour-of-rust/gcd-web/src/gcd.rs
@@ -0,0 +1,18 @@
+pub fn gcd(mut m: u64, mut n: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2 * 3 * 5 * 11 * 17, 3 * 7 * 11 * 13 * 19), 3 * 11,);
+}
